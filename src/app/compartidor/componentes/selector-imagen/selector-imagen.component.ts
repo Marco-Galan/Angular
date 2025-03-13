6051ec26cfa9fafc5562c4fb22fc46ab31f517e5
Marco <marco_rg@msn.com>
2025-03-12 18:01:47 -0600

    Control para seleccionar imagenes

---
+++ src/app/compartidor/componentes/selector-imagen/selector-imagen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-imagen',
+  imports: [],
+  templateUrl: './selector-imagen.component.html',
+  styleUrl: './selector-imagen.component.css'
+})
+export class SelectorImagenComponent {
+
+}

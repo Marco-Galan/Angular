40ea96d90226d6371870f5ecd32eafab8181fdc4
Marco <marco_rg@msn.com>
2025-03-12 19:30:53 -0600

    Inicio Formulario Cines

---
+++ src/app/cines/formulario-cines/formulario-cines.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-cines',
+  imports: [],
+  templateUrl: './formulario-cines.component.html',
+  styleUrl: './formulario-cines.component.css'
+})
+export class FormularioCinesComponent {
+
+}

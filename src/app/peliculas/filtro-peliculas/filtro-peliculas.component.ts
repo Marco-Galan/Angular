652957892ea7abc5a1d28dfde016026a0e273906
Marco <marco_rg@msn.com>
2025-03-20 15:54:45 -0600

    Se agregará filtro de peliculas

---
+++ src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-peliculas',
+  imports: [],
+  templateUrl: './filtro-peliculas.component.html',
+  styleUrl: './filtro-peliculas.component.css'
+})
+export class FiltroPeliculasComponent {
+
+}

30ccd5a6d5f7756ed3e648158cbac6e723a0dc0d
Marco <marco_rg@msn.com>
2025-03-21 22:35:21 -0600

    Inicio Componente Autocompletado de actores

---
+++ src/app/actores/autocompletado-actores/autocompletado-actores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocompletado-actores',
+  imports: [],
+  templateUrl: './autocompletado-actores.component.html',
+  styleUrl: './autocompletado-actores.component.css'
+})
+export class AutocompletadoActoresComponent {
+
+}
